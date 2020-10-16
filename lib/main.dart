0fc71225ac59c52424b186e833d6575b25bcfe6e
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-10-15 18:34:27 -0600

    feat(state): Add state to dice - func

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import "dart:math";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+          shadowColor: Colors.transparent,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(7);
+                  print('diceNumber = $leftDiceNumber');
+                });
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(7);
+                  print('diceNumber = $rightDiceNumber');
+                });
+              },
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
